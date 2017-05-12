099d46d57a3f450c531393803821c8a78376f462
Kirk Byers <ktbyers@twb-tech.com>
2017-05-12 15:39:50 -0700

    Change save directory

--- netmiko/save_output.py
+++ netmiko/save_output.py
@@ -5,13 +5,14 @@ from my_devices import device_list
 
 
 command = "show run"
+base_dir = "/home/kbyers/configs"
 
 for device in device_list:
     net_connect = ConnectHandler(**device)
 
     # Determine name of device
     hostname = net_connect.base_prompt
-    filename = "./configs/{}.txt".format(hostname)
+    filename = "{}/{}.txt".format(base_dir, hostname)
 
     # Send show command down channel
     print("Retrieving output: {}".format(hostname))
